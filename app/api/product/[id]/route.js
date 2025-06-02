10e2a3aa50bc408be6ebe26a6410d64adda685e5
Le <ITCSIU22271@student.hcmiu.edu.vn>
2025-06-03 01:00:10 +0700

    update

--- app/api/product/[id]/route.js
+++ app/api/product/[id]/route.js
@@ -2,18 +2,30 @@ import prisma from "@/app/libs/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(req, context) {
-    try {
-        const { id } = context.params
+    // try {
+    //     const { id } = context.params
 
-        const product = await prisma.products.findFirst({
-            where: { id: Number(id) }
-        })
+    //     const product = await prisma.products.findFirst({
+    //         where: { id: Number(id) }
+    //     })
         
-        await prisma.$disconnect();
-        return NextResponse.json(product);
-    } catch (error) {
-        console.log(error);
-        await prisma.$disconnect();
-        return new NextResponse('Something went wrong', { status: 400 });
-    }
+    //     await prisma.$disconnect();
+    //     return NextResponse.json(product);
+    // } catch (error) {
+    //     console.log(error);
+    //     await prisma.$disconnect();
+    //     return new NextResponse('Something went wrong', { status: 400 });
+    // }
+    try {
+        // Await the context.params to access its properties properly
+        const { id } = await context.params; // Use await here to get params correctly
+    
+        const product = await prisma.products.findFirst({
+          where: { id: Number(id) },
+        });
+    
+        return new Response(JSON.stringify(product));
+      } catch (error) {
+        return new Response('Error fetching product', { status: 500 });
+      }
 }
